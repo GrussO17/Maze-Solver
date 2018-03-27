f555ccf2409a463230aad6f1b94e364eadf37c6a
Owen Michael Gruss <omg6429@cs.rit.edu>
2018-03-27 17:15:11 -0400

    fixed errors and warnings, segfault occurse when ran. fixed pointer errors and syntax things in mopsolver.c

--- mopsolver.c
+++ mopsolver.c
@@ -41,22 +41,20 @@ void destroy(QNode* que){
 	free(que);
 }
 
-int breadthFirst(int size, int maze[][size]){
-	int shortest;
-	int visited[size][size] = {{0}};
+int breadthFirst(int size, int rows, int maze[][size], int visited[][size]){
 	QNode* que = malloc(sizeof(QNode));
 	que->x = 0;
 	que->y = 0;
 	que->pathSize = 0;
 	que->next = NULL;
 	QNode* temp;
-	size_t x;
-	size_t y;
+	int x;
+	int y;
 	int pS;
 	do{
 		temp = dequeue(que);
-		x = temp->x;
-		y = temp->y;
+		x = (int)temp->x;
+		y = (int)temp->y;
 		pS = temp->pathSize;
 		free(temp);
 		if(x != 0 && maze[y][x - 1] == OPEN && 
@@ -74,31 +72,17 @@ int breadthFirst(int size, int maze[][size]){
                         visited[y - 1][x] = 1;
                         enqueue(que, x, y - 1, pS++);
                 }
-		if(y != size-1 && maze[y + 1][x] == OPEN &&
+		if(y != rows-1 && maze[y + 1][x] == OPEN &&
                 visited[y + 1][x] != 1){
                         visited[y + 1][x] = 1;
                         enqueue(que, x, y + 1, pS++);
                 }
-	}while(x !=  size - 1 && y != size-1)
-	destoy(que);
+	}while(x !=  size - 1 && y != size-1);
+	destroy(que);
 	return pS;
-
-
-
-
 }
 
-/**
-int** createTrail(int size){
-	int* trail = malloc(sizeof(int)*size*size);
-	for(int i = 0; i < size; i++){
-		for(int j = 0; j < size; j++){
-			trail[i][j] = 0;
-		}
-	}
-}
-*/
-int depthSearch(int size, int maze[][size], int visited[][size],
+int depthSearch(int size, int rows, int** maze, int visited[][size],
 	 int x, int y, int done){
 	
 	if(x == size-1 && y == size-1){
@@ -107,47 +91,64 @@ int depthSearch(int size, int maze[][size], int visited[][size],
 	}
 	else if(done == 0){
 		visited[x][y] = 1;
-		if(x != 0 && !visited[x - 1][y])
-			depthSearch(size, maze, visited, x - 1, y, done);
-		if(y != 0 && !visited[x][y - 1])
-		        depthSearch(size, maze, visited, x, y - 1, done);
-		if(x != size -1  && !visited[x + 1][y])
-			depthSearch(size, maze, visited, x + 1, y, done);
-		if(y != size -1  && !visited[x][y + 1])
-			depthSearch(size, maze, visited, x, y + 1, done);
+		if(x != 0 && visited[x - 1][y] != 1)
+			depthSearch(size, rows, maze, visited, x - 1, y, done);
+		if(y != 0 && visited[x][y - 1] != 1)
+		        depthSearch(size, rows, maze, visited, x, y - 1, done);
+		if(x != size -1  && visited[x + 1][y] != 1)
+			depthSearch(size, rows, maze, visited, x + 1, y, done);
+		if(y != size -1  && visited[x][y + 1] != 1)
+			depthSearch(size, rows, maze, visited, x, y + 1, done);
 	}
+	return 0;	
 }
 
 
-int** readFile(FILE* pfile, int* returnSize){
+int** readFile(FILE* pfile, int* returnCols, int* returnRows){
 	FILE* temp = pfile;
-	int counter = 0;
-	char ret;
-	while(fscanf(temp, "%c", &ret) != '\n')
+	int counter = -1;
+	char character = 'a';
+	while( character != '\n'){
+		fscanf(temp, "%c", &character);
 		counter++;
-	int size = counter;
-	int ret** = malloc(sizeof(int) * size);
-	char* next = 'a';
-	int numrows = 1;
-	for( int j = 0; *next != EOF; j++){
-		numrows ++;
-		if( j != 0 )
-			ret = realloc(ret, sizeof(int) * (size) * numrows); 
-		for( int i = 0; i < size; i++){
-			fscanf( pfile, "%i", ret[j][i]); 
-		}
-		fscanf(pfile, "%c", next);
 	}
-	returnSize = size;
+	printf(" width %d", counter);
+	int size = counter;
+	int** ret;
+	ret = (int**)malloc(sizeof(int) * size);
+	char next;
+	next = 'a';
+	int numrows = 1;
+	int num = 0;
+	for( int j = 0; next != EOF; j++){
+		numrows ++;
+		if( j != 0 ){
+		       ret = (int**)realloc(ret, sizeof(int) 
+			* (size) * numrows); 
+		}
+		for( int i = 0; i < size; i++){
+			fscanf( pfile, "%i", &num);
+			ret[j][i] = num;
+			printf("%d\n", num);
+		}
+		fscanf(pfile, "%c", &next);
+	}
+	*returnCols = size;
+	*returnRows = numrows;
 	return ret;
 }
 
 int main(int argc, char ** argv){
+	int* returnCols = 0;
+	int* returnRows = 0;
 	if(argc != 2) 
 		return EXIT_FAILURE;
 	FILE* pFile = fopen(argv[1], "r");
-	int returnSize;
-	int** maze = readFile(pFile, returnSize);
-	fprintf("%i", returnSize);
-
+	int** maze = readFile(pFile, returnCols, returnRows);
+	int visited[*returnRows][*returnCols];
+        for(int a = 0; a < *returnRows; a++)
+                for(int b = 0; b < *returnCols; b++)
+                        visited[a][b] = 0;
+	int canComplete = depthSearch(*returnCols, *returnRows, maze, visited, 0, 0, 0);
+	printf("can be complete? %d", canComplete);
 }
